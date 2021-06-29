f34ca6c7b2fd3e031bc965b7937db67a3aeca803
muneebmemon <muneeb.memon@gmail.com>
2021-06-29 18:04:54 -0500

    completed

--- samplevueproject/src/components/SubmissionForm.vue
+++ samplevueproject/src/components/SubmissionForm.vue
@@ -25,16 +25,22 @@
       <button type="submit"
             @click="submitForm">Submit
     </button>
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+    </ul>
+  </p>
     </div>
   </div>
 </template>
 
 <script>
-const axios = require('axios');
 export default {
   name: 'SubmissionForm',
   data() {
             return {
+                errors: [],
                 userData: {
                     firstname: '',
                     lastname: '',
@@ -44,12 +50,42 @@ export default {
         },
 
   methods: {
-      submitForm() {
-          axios.post(this.endpoint, {body: this.userData})
-               .then(({data}) => {
-                      
-                    });
-            }
+    validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+},
+      submitForm(e) {
+        e.preventDefault();
+        this.errors = [];
+        if (!this.userData.firstname) {
+          this.errors.push('First Name required.');
+        }
+        if (!this.userData.lastname) {
+          this.errors.push('Last Name required.');
+        }
+        if (!this.userData.email) {
+          this.errors.push('Email required.');
+        }
+
+        if (this.userData.email && !this.validateEmail(this.userData.email)) {
+          this.errors.push('Email should be in the format name@company.com');
+        }
+
+        if (this.errors.length===0) {
+          fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: this.userData.firstname,
+    body: this.userData.lastname + ' ' + this.userData.email,
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then(() => alert('Thank you. Your info was successfully submitted'));
+            }}
         }
 }
 </script>
