2f3822724f93982c4c089440fa016a7747337337
muneebmemon <muneeb.memon@gmail.com>
2021-06-28 19:04:30 -0500

    started working

---
+++ samplevueproject/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <SubmissionForm />
+</template>
+
+<script>
+import SubmissionForm from './components/SubmissionForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SubmissionForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ samplevueproject/src/components/SubmissionForm.vue
@@ -0,0 +1,62 @@
+<template>
+<h3> Please submit your info: </h3>
+<br/>
+  <div class="submissionform">
+    <div class="form-group">
+      <div class="field">
+        <label class="label">First Name:</label>
+        <div class="control">
+          <input v-model="userData.firstname" class="input" type="text" placeholder="First Name">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Last Name:</label>
+        <div class="control">
+          <input v-model="userData.lastname" class="input" type="text" placeholder="Last Name">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Email:</label>
+        <div class="control">
+          <input v-model="userData.email" class="input" type="text" placeholder="Email">
+        </div>
+      </div>
+      <br/>
+      <button type="submit"
+            @click="submitForm">Submit
+    </button>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+export default {
+  name: 'SubmissionForm',
+  data() {
+            return {
+                userData: {
+                    firstname: '',
+                    lastname: '',
+                    email: ''
+                }
+            }
+        },
+
+  methods: {
+      submitForm() {
+          axios.post(this.endpoint, {body: this.userData})
+               .then(({data}) => {
+                      
+                    });
+            }
+        }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
